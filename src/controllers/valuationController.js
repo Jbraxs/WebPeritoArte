86513056508ae0bd8212dbbba3962b51c7d8c427
Jonatan <jonatanbraxs@gmail.com>
2018-09-01 02:54:49 +0200

    ctrl valoraciones 0.2

--- src/controllers/valuationController.js
+++ src/controllers/valuationController.js
@@ -10,8 +10,7 @@ controllerValuation.selectValuation = (req, res) => {
         connection.query("SELECT * FROM objeto where idUsuario = ?", req.session.user.id, (err, valuations) => {
             if (err) {
                 res.json(err);
-            } console.log(valuations);
-
+            } 
             res.render("zonacliente/valuations", {
                 valuations: valuations
             })
@@ -22,68 +21,54 @@ controllerValuation.selectValuation = (req, res) => {
 controllerValuation.addValuationForm = (req, res) => {
     req.session.user = { id: 39, nombre: '2', email: '2' };
     req.getConnection((err, connection) => {
-        connection.query("SELECT * FROM categoria; SELECT * FROM tecnica; ", (err, results) => {
-            console.log(results);
-        
-
-                // res.render("zonacliente/valuations_add", {
-                    // categorias: results[0],
-                    // tecnicas: results[1],
-                    // tamanios: results[2]
-                // });
+        connection.query('SELECT * FROM categoria', (err, categorias) => {
+            connection.query('SELECT * FROM tipo_objeto', (err, tipo_objetos) => {
+                connection.query('SELECT * FROM tecnica', (err, tecnicas) => {
+                    connection.query('SELECT * FROM tamanio', (err, tamanios) => {
+                        connection.query('SELECT * FROM conservacion', (err, conservacions) => {
+                            res.render("zonacliente/valuations_add", {
+                                categorias: categorias,
+                                tipo_objetos: tipo_objetos,
+                                tecnicas: tecnicas,
+                                tamanios: tamanios,
+                                conservacions: conservacions
+                            });
+                        });
+                    });
+                });
+            });
         });
     });
 };
-    // else {
-    //     categorias = result;         
-    //     tipo_objeto = result2;
-    //     // res.render("zonacliente/valuations_add", {categorias:categorias, });
-    // }
 
-    // var categorias = result;
-    // console.log(result);
-    // console.log(categorias)
-    //     connection.query("SELECT * FROM tamanio", (err, tamanios) => {
-    //     if(err) {
-    //             res.json(err);
-    //         }   
-    //     })
-    //     res.render("zonacliente/valuations_add", {tamanios:tamanios});
-    // })
-    // req.getConnection((err, connection) => {
-    //     connection.query("SELECT * FROM tecnica", (err, tecnicas) => {
-    //     if(err) {
-    //             res.json(err);
-    //         }   
-    //     })
-    // })
-    // res.render("zonacliente/valuations_add", {categorias:categorias,tamanios:tamanios,tecnicas:tecnicas});
-    // res.render("zonacliente/valuations_add", {categorias:categorias});
-
-    controllerValuation.addValuation = (req, res) => {
-        // const usuario = req.session.user;
-        // let oldPath = req.files.imagenes.path;
-        // let newPath = '/src/public/img_clientes/' + req.files.imagenes.originalFilename;
-        // fs.rename(oldPath,newPathm function (err){
-        // });
-        // const data = req.body;
-        req.getConnection((err, connection) => {
-            let sql = `INSERT INTO objeto (idUsuario,nombre,
-        idCategoria,
-        idTipoObjeto,idTecnica) VALUES 
-       ('${usuario.id}','${req.body.nombre}','${req.body.idTipoObjeto}','${req.body.idTecnica}')`;
-            connection.query(sql, (err, valoraciones) => {
-                // 'INSERT INTO objeto set ? ?', usuario , data,
-                if (err) {
-                    console.log(err);
-                    return res.send("error al valorar");
-                }
-                res.redirect('/');
-            })
+// const usuario = req.session.user;
+// let oldPath = req.files.imagenes.path;
+// let newPath = '/src/public/img_clientes/' + req.files.imagenes.originalFilename;
+// fs.rename(oldPath,newPathm function (err){
+// });
+// const data = req.body;
+controllerValuation.addValuation = (req, res) => {
+    req.session.user = { id: 39, nombre: '2', email: '2' };
+    const usuario = req.session.user;
+    req.getConnection((err, connection) => {
+        let sql = `INSERT INTO objeto (idUsuario,nombre,idCategoria,idTipoObjeto,idTecnica,idTamanio,firmado,comentario,conservacion) VALUES 
+        ('${usuario.id}','${req.body.nombre}','${req.body.categoria_id}','${req.body.tipoObjeto_id}','${req.body.tecnica_id}','${req.body.tamanio_id}',
+        '${req.body.firmado_id}','${req.body.comentario}','${req.body.conservacion_id}')`;
+        // console.log(usuario.id);
+        // console.log(req.body.nombre);
+        console.log(sql);
+        connection.query(sql, (err, valoraciones) => {
+            if (err) {
+                // console.log(err);
+                return res.send("error al valorar");
+            }
+            // console.log(valoraciones);
+            res.redirect('/zonacliente/valuations');
         })
-    };
+    })
+};
 
 
 
 
-    module.exports = controllerValuation;
+module.exports = controllerValuation;
